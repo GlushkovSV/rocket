746eb8063c7fba55e6b857ba4940fbcd15330e19
GlushkovSV <GlushkovSergeyV@gmail.com>
2023-01-23 01:55:39 +0400

    Update class of stage

--- stage.py
+++ stage.py
@@ -1,4 +1,21 @@
-class STAGE():
+class STAGE:
     def __init__(self):
-        pass
+        self.parts = list()
 
+    def add_part(self, part):
+        self.parts.append(part)
+
+    def mass(self) -> float:
+        """
+        не python-way переписать. Возможно лучше так
+        return sum(part.mass() for part in self.parts)
+
+        :return:
+        """
+        total_mass = 0
+        for part in self.parts:
+            total_mass += part.mass()
+        return total_mass
+
+    def __str__(self):
+        return '\n'.join(str(part) for part in self.parts)
